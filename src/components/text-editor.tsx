1b38f447e814bbf731928145fa6eeaac637e8788
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2026-02-05 18:42:19 +0700

    update

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,6 +1,6 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import Placeholder from '@tiptap/extension-placeholder';
+import Placeholder from "@tiptap/extension-placeholder";
 import { Box, IconButton, Tooltip, Sheet } from "@mui/joy";
 import {
   Bold,
